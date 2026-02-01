cad3297529751ae1aaf35ad55b67f36d9ad3c1d3
habibe <habibetopal46@gmail.com>
2026-02-01 13:39:37 +0300

    Pointer kullanımı, adres alma ve pointer üzerinden değişkene erişimi gösteren örnek çalışma.
    Bir değişkenin adresinin alınması pointer değişkeni ile bu adresin tutulması ve * yönlendirme operatörü kullanılarak işaret edilen değerin değiştirilmesi gösterilmiştir. Örnek üzerinden, pointer ile yapılan değişikliğin asıl değişkenin değerini nasıl etkilediği açıklanmıştır.

---
+++ Pointer.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int t = 13;
+
+    int* i = &t;    // & adres operatörü
+
+    /*cout << "t'nin degeri: " << t << "\n" << endl;
+    cout << "t'nin adresi: " << &t << "\n" << endl;
+    cout << "i'nin degeri: " << i << "\n" << endl;
+    cout << "i'nin adresi: " << &i << "\n" << endl;*/
+
+    cout << "--------------------------------------------------------\n" << endl;
+
+    // * yönlendirme operatörü
+    // işaretçi (pointer) üzerinden işaret edilen değişkene erişim sağlanır
+
+    *i = 2024;
+
+    cout << "t'nin degeri: " << t << "\n" << endl;
+    cout << "t'nin adresi: " << &t << "\n" << endl;
+    cout << "i'nin degeri: " << i << "\n" << endl;
+    cout << "i'nin adresi: " << &i << "\n" << endl;
+    cout << "i uzerinden erisilen deger: " << *i << "\n" << endl;
+
+}
